200bec8fa4a1ab20c1c0fa9d27e6698a31855708
marwen <marwen.saidi@orange.com>
2023-04-03 08:05:40 +0200

    preparing

--- src/test/java/kata/PasswordTest.java
+++ src/test/java/kata/PasswordTest.java
@@ -1,41 +1,11 @@
 package kata;
 
-import org.junit.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class PasswordTest {
     private Password password = new Password();
 
-    @Test
-    public void
-    validates_length_letter_and_number() {
-        assertThat(password.check("")).contains(
-                "Password needs to be 7 chars long",
-                "Password needs to contain atleast one digit",
-                "Password must contain a letter");
-    }
-
-    @Test
-    public void
-    passwords_are_valid_when_they_have_letter_number_and_are_atleast_7_chars_long() throws Exception {
-        assertThat(password.check("helpme7")).isEmpty();
-    }
-
-    @Test
-    public void
-    admins_validates_length_letter_and_number() throws Exception {
-        assertThat(password.checkAdmin("")).contains(
-                "Password needs to be 10 chars long",
-                "Password needs to contain atleast one digit",
-                "Password must contain a letter",
-                "Password needs to contain atleast one special character");
-    }
-
-    @Test
-    public void
-    admin_passwords_passes_when_they_have_everything() throws Exception {
-        assertThat(password.checkAdmin("10helpme..s")).isEmpty();
-    }
+    //TODO: le password doit avoir 7 caractère, au moins un chiffre et au moins une lettre
+    //TODO: Tester un password valide (qu'il retourne aucun message d'erreur
+    //TODO: le pass du role admin doit avoir 10 caractère, au moins un chiffre, au moins une lettre et au moins un caractère spécial
+    //TODO: Tester un password admin valide (qu'il retourne aucun message d'erreur
 
 }
