82556ab5abef65a2f4f7d03cf3ceedd38e903a33
marwen <marwen.saidi@orange.com>
2023-04-05 17:15:25 +0200

    fix

--- src/test/java/kata/ValidatorTest.java
+++ src/test/java/kata/ValidatorTest.java
@@ -5,7 +5,7 @@ public class ValidatorTest {
     //TODO: à tester :
 //        - si contient lettre ou lettre accentué ==> OK
 //        - si pass est vide ou contient qu'un chiffre retourner le msg d'erreur qu'il doit contenir une lettre
-//        - si même demande que le point 2 mais pour un chiffre cette fois.
+//        - si même demande que le point 2 mais pour un lettre cette fois.
 //        - si contient chiffre => OK
 //        - deux test un KO et un OK pour respectivement une longueur de pass moins ou égale / plus que 7
 //        - deux test pour le cas admin KO et OK pour respectivement ne contient et contient un caractère spécial
